ef170396bd648fbb8d8cc6d5010fc9e6bc14d9ec
Lydia Astrella <lydiaastrella@gmail.com>
2020-04-13 18:29:49 +0700

    change main acitivity UI

--- app/src/main/java/com/example/eventandguest/MainActivity.java
+++ app/src/main/java/com/example/eventandguest/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
 
         editName = findViewById(R.id.edit_name);
         editPalindrom = findViewById(R.id.edit_palindrom);
